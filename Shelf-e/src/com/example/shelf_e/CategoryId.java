54a7ce63eb593b628bedc81f400be09cb69e864b
ChancellorRoach <sac.roach@gmail.com>
2014-07-16 23:35:24 -0400

    app

---
+++ Shelf-e/src/com/example/shelf_e/CategoryId.java
@@ -0,0 +1,15 @@
+package com.example.shelf_e;
+
+public class CategoryId {
+
+	public static int BABY_FOOD = 1;
+	public static int BAKING  = 2;
+	public static int BEER = 3;
+	public static int BEVERAGES = 4;
+	public static int DAIRY = 5;
+	public static int FRUITS = 6;
+	public static int BREAD = 7;
+	public static int VEGETABLES = 8;
+	
+
+}
