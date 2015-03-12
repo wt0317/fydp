588479f756a15626f12caef307bca69ff367e5ce
Sieyor Roach <saroach@uwaterloo.ca>
2015-03-12 14:09:46 -0400

    added files

---
+++ Shelfe/src/com/fydp/shelfe/User.java
@@ -0,0 +1,31 @@
+package com.fydp.shelfe;
+
+import com.fydp.shelfe.R.drawable;
+
+public class User {
+
+	private static String username;
+	private static String password;
+	
+
+	public static String getUsername() {
+		return username;
+	}
+
+
+	public static void setUsername(String username) {
+		User.username = username;
+	}
+
+
+	public static String getPassword() {
+		return password;
+	}
+
+
+	public static void setPassword(String password) {
+		User.password = password;
+	}
+
+
+}
